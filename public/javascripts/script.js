e22bfa0e90641763c3711f7f1cd370109092ea7b
Jesus Rua <jesus.rua.pareja@gmail.com>
2020-11-22 13:38:30 +0100

    CITIES SORTED

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,15 +16,43 @@ const getCityList = () => {
       })
         .then((result) => {
           const cities = result.data.results
-          let capitals = []
-
           
+          const capitals = []
+          cities.forEach(city => {
+            capitals.push(city.Subdivision_Name)
+          });
 
-          cities.forEach((city) => {
+          const capitalsFixed = [...capitals]
+
+          for(let i = 0; i < capitalsFixed.length; i++) {
+            capitalsFixed[i] = capitalsFixed[i].replace("_", "é");
+            capitalsFixed[i] = capitalsFixed[i].replace("Biskaia", "Bilbo");
+            capitalsFixed[i] = capitalsFixed[i].replace("La Rioja", "Logroño");
+            capitalsFixed[i] = capitalsFixed[i].replace("Gipuzkoa", "Donosti");
+            capitalsFixed[i] = capitalsFixed[i].replace("Las Palmas", "Las Palmas de Gran Canaria");
+            capitalsFixed[i] = capitalsFixed[i].replace("Álava / Araba", "Gasteiz");
+            capitalsFixed[i] = capitalsFixed[i].replace("Balears  [Baleares]", "Palma de Mallorca");
+            capitalsFixed[i] = capitalsFixed[i].replace("Navarra / Nafarroa", "Pamplona");
+            capitalsFixed[i] = capitalsFixed[i].replace("Alicante / Alacant", "Alacant");
+            capitalsFixed[i] = capitalsFixed[i].replace("A Coruña  [La Coruña]", "A Coruña");
+            capitalsFixed[i] = capitalsFixed[i].replace("Tarragona  [Tarragona]", "Tarragona");
+            capitalsFixed[i] = capitalsFixed[i].replace("Castellón / Castelló", "Castelló");
+            capitalsFixed[i] = capitalsFixed[i].replace("Valencia / València", "València");
+            capitalsFixed[i] = capitalsFixed[i].replace("Lugo  [Lugo]", "Lugo");
+            capitalsFixed[i] = capitalsFixed[i].replace("Barcelona  [Barcelona]", "Barcelona");
+            capitalsFixed[i] = capitalsFixed[i].replace("Ourense  [Orense]", "Ourense");
+            capitalsFixed[i] = capitalsFixed[i].replace("Girona  [Gerona]", "Girona");
+            capitalsFixed[i] = capitalsFixed[i].replace("Pontevedra  [Pontevedra]", "Pontevedra");
+            capitalsFixed[i] = capitalsFixed[i].replace("Lleida  [Lérida]", "Lleida");
+          }
+
+          const sortedCapitals = capitalsFixed.sort()
+
+          sortedCapitals.forEach((city) => {
 
             const option = document.getElementById('cities-full-list')
             const capital = document.createElement('option')
-            const capitalText = document.createTextNode(`${city.Subdivision_Name}`)
+            const capitalText = document.createTextNode(`${city}`)
             
             capital.appendChild(capitalText)
             option.appendChild(capital)
